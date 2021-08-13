7d3e53effb9e30e2e71e33a3d5398c6ade246ab0
Мусин Михаил <musin.mihail@gmail.com>
2021-08-13 21:17:04 +0300

    Split3

--- Assets/SplitRange.cs
+++ Assets/SplitRange.cs
@@ -9,50 +9,38 @@ public class SplitRange : MonoBehaviour
 {
     void Start()
     {
-        // List<string> cauntryName = new List<string>();
-        // List<string> SplitRange = new List<string>();
         string oldIp = "";
         string newIp = "";
         string[] AllRange = File.ReadAllLines("DBIP.txt");
         foreach (string item in AllRange)
         {
             List<string> numbers2 = item.Split(',').ToList();
-            if (numbers2[2] == "FR")
+            List<int> startIp = IPAddress.Parse(numbers2[0]).ToString().Split('.').Select(Int32.Parse).ToList();
+            List<int> endIp = IPAddress.Parse(numbers2[1]).ToString().Split('.').Select(Int32.Parse).ToList();
+            oldIp = IPAddress.Parse(numbers2[0]).ToString();
+            if(startIp[1] != endIp[1])
             {
-                List<int> startIp = IPAddress.Parse(numbers2[0]).ToString().Split('.').Select(Int32.Parse).ToList();
-                List<int> endIp = IPAddress.Parse(numbers2[1]).ToString().Split('.').Select(Int32.Parse).ToList();
-                oldIp = IPAddress.Parse(numbers2[0]).ToString();
-                if(startIp[1] != endIp[1])
+                int count = endIp[1] - startIp[1];
+                for (int i = 0; i < count + 1; i++)
                 {
-                    // Debug.Log(startIp[0] + "." + startIp[1] + "." + startIp[2] + "." + startIp[3]);
-                    // Debug.Log(endIp[0] + "." + endIp[1] + "." + endIp[2] + "." + endIp[3]);
-                    int count = endIp[1] - startIp[1];
-                    // Debug.Log(count);
-                    for (int i = 0; i < count + 1; i++)
+                    if(i == count)
                     {
-                        if(i == count)
-                        {
-                            newIp = startIp[0] + "." + (startIp[1]+i) + "." + endIp[2] + "." + endIp[3];
-                            File.AppendAllText(numbers2[3] + ".txt", oldIp + "." + newIp + "\n");
-                            // SplitRange.Add(oldIp + " - " + newIp + ";" + numbers2[4]);
-                            oldIp = startIp[0] + "." + (startIp[1]+i+1) + "." + 0 + "." + 0;
-                        }
-                        else
-                        {
-                            newIp = startIp[0] + "." + (startIp[1]+i) + "." + 255 + "." + 255;
-                            File.AppendAllText(numbers2[3] + ".txt", oldIp + "." + newIp + "\n");
-                            // SplitRange.Add(oldIp + " - " + newIp + ";" + numbers2[4]);
-                            oldIp = startIp[0] + "." + (startIp[1]+i+1) + "." + 0 + "." + 0;
-                        }
+                        newIp = startIp[0] + "." + (startIp[1]+i) + "." + endIp[2] + "." + endIp[3];
+                        File.AppendAllText(numbers2[3] + ".txt", oldIp + "." + newIp + "\n");
+                        oldIp = startIp[0] + "." + (startIp[1]+i+1) + "." + 0 + "." + 0;
+                    }
+                    else
+                    {
+                        newIp = startIp[0] + "." + (startIp[1]+i) + "." + 255 + "." + 255;
+                        File.AppendAllText(numbers2[3] + ".txt", oldIp + "." + newIp + "\n");
+                        oldIp = startIp[0] + "." + (startIp[1]+i+1) + "." + 0 + "." + 0;
                     }
                 }
-                else
-                {
-                    File.AppendAllText(numbers2[3] + ".txt", IPAddress.Parse(numbers2[0]).ToString() + "." + IPAddress.Parse(numbers2[1]).ToString() + "\n");
-                    // SplitRange.Add(IPAddress.Parse(numbers2[0]).ToString() + ";" + IPAddress.Parse(numbers2[1]).ToString() + ";" + numbers2[4]);
-                }            
             }
+            else
+            {
+                File.AppendAllText(numbers2[3] + ".txt", IPAddress.Parse(numbers2[0]).ToString() + "." + IPAddress.Parse(numbers2[1]).ToString() + "\n");
+            }            
         }
-        // File.WriteAllLines("FR.txt", SplitRange);
     }
 }
